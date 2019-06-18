1253ddaa80c73b6d9a1b453d4a010f5a02b4555b
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-06-18 15:41:10 -0400

    for fun  user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+before_action :find_user, except: [:index]
+before_action :all_user, only: [:index]
+
+def index
+
+end
+
+def show
+
+end
+
+def edit
+
+end
+
+
+
+private
+def find_user
+  @user = User.find(params[:id])
+end
+
+def all_user
+  @users = User.all
+end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :responses
   has_many :events, through: :responses
+  # validates :
 end
