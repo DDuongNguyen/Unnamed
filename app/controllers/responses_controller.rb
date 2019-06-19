7401a2352595f64019eb8e4f1fcc725e775bd07d
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-06-19 16:36:37 -0400

    RSVPING | SITE HEADERS | ROOT PAGE REDIRECTgit add .git add .!

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,24 @@
 class ResponsesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    Response.create(event_id: params[:id], user_id: @current_user.id)
+    redirect_to "/users/#{@current_user.id}"
+  end
+
+  def destroy
+    # byebug
+    rsvp = Response.find_by(event_id: params[:id], user_id: @current_user.id)
+    rsvp.destroy
+    redirect_to "/users/#{@current_user.id}"
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:user_id, :event_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "events#index"
   resources :events
   resources :responses
   resources :users
@@ -7,6 +8,17 @@ Rails.application.routes.draw do
   get '/login/new', to: 'login#new'
   post '/login/new', to: 'login#create'
   delete '/logout', to:'login#destroy'
+
+  # make a custom routes to RSVP in event show page
+  post "/events/:id/create", to: 'responses#create'
+  delete "/users/:id/delete", to: 'responses#destroy'
+
+
+
+
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
